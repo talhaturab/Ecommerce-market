482f5553e533d369f4430141d85cb46b512a4730
Talha Turab <turabtalha@gmail.com>
2022-09-26 13:06:22 +0500

    Initial setup with HTML CSS

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import "../App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Shop from "./Shop";
+
+function App() {
+	return (
+		<>
+			<Header />
+			{/* <Home /> */}
+			<Shop />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ src/utils/DATA.js
@@ -0,0 +1,144 @@
+let products = {
+	0: {
+		id: "0",
+		name: "Colorful Stylish Shirt",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["female", "shirt"],
+		price: 150,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    1: {
+		id: "1",
+		name: "children shirt",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["children", "shirt"],
+		price: 400,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+    2: {
+		id: "2",
+		name: "stylish jacket",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["male", "jacket"],
+		price: 1000,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    3: {
+		id: "3",
+		name: "long shirt",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["female", "shirt"],
+		price: 200,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    4: {
+		id: "4",
+		name: "children styish shirt",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["children", "shirt"],
+		price: 50,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    5: {
+		id: "5",
+		name: "men suit",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["men", "suit"],
+		price: 700,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    6: {
+		id: "6",
+		name: "stylish coat",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["women", "coat"],
+		price: 400,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    7: {
+		id: "7",
+		name: "baby shirt",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["baby", "shirt"],
+		price: 100,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+
+    8: {
+		id: "8",
+		name: "exquisite shirt for woman",
+		description:
+			"Volup erat ipsum diam elitr rebum et dolor. Est nonumy elitr erat diam stet sit clita ea. Sanc invidunt ipsum et, labore clita lorem magna lorem ut. Erat lorem duo dolor no sea nonumy. Accus labore stet, est lorem sit diam sea et justo, amet at lorem et eirmod ipsum diam et rebum kasd rebum.",
+		catogory: ["women", "shirt"],
+		price: 30,
+		availability: {
+			xs: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            s: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            m: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            l: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+            xl: { black: 5, white: 10, red: 15, blue: 20, green: 25 },
+		},
+	},
+};
